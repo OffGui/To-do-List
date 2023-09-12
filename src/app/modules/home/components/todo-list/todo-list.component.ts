5c75d1f443c5b42bf3653b4f3269b35c7b600e1c
Guilherme Prata <offgui023@gmail.com>
2023-09-12 17:16:42 -0300

    commit aula Hands-on 10

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { TaskList } from '../../model/task-list';
 
 @Component({
@@ -6,25 +6,36 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements DoCheck{
 
-  public taskList: Array<TaskList> = [
-    { task: "Minha nova task", checked: true },
-    { task: "task2", checked: false }
-  ];
-  constructor() { }
+  public taskList: Array<TaskList> = [];
 
-  ngOnInit(): void {
+  constructor() { } 
+
+  ngDoCheck(){
+    this.taskList.sort( (first, last) => Number(first.checked) - Number(last.checked));
   }
 
   public deleteItemTaskList(event: number){
     this.taskList.splice(event, 1);
   }
-
+  public setEmitTaskList(event: string){
+    this.taskList.push({ task: event, checked: false });
+  }
   public deletAllTaskList(){
     const confirm = window.confirm("Você deseja realmente Deletar tudo?");
     if (confirm) {
       this.taskList = [];
+    }
+  }
+  
+  public validationInput(event: string, index: number){
+    if(!event.length){
+      const confirm = window.confirm("Task está vazia, deseja Deletar?");
+
+      if(confirm){
+        this.deleteItemTaskList(index);
+      }
     }
   }
 }
